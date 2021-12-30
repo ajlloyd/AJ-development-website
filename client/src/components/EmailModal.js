edf4d3afac2909f56792f48293d66365f0fff0ec
ajlloyd <ajlloyd98@gmail.com>
2021-12-30 23:17:33 +0000

    edited modal

--- client/src/components/EmailModal.js
+++ client/src/components/EmailModal.js
@@ -2,20 +2,26 @@ import React from 'react'
 import { Modal, Button, CloseButton } from 'react-bootstrap'
 
 const EmailModal = (props, body) => {
+
+    
+    var styles1 = {
+        color: 'black'
+    };
+
     
     return (
         <div>
             <Modal {...props} size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
                 <Modal.Header>
                     
-                    <Modal.Title id="contained-modal-title-vcenter">
-                        Modal heading
+                    <Modal.Title style={styles1} id="contained-modal-title-vcenter">
+                        Email Sent
                     </Modal.Title>
                     <CloseButton  onClick={props.onHide}/>
                 </Modal.Header>
                 <Modal.Body>
-                    <h4>Centered Modal</h4>
-                    <p>poo hole</p>
+                    <h3 style={styles1}>Success 🥳 !</h3>
+                    <h5 style={styles1}>Thank you for submitting your request - I will respond as soon as possible.</h5>
                 </Modal.Body>
                 <Modal.Footer>
                     <Button onClick={props.onHide}>Close</Button>
