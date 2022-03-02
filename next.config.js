0fee50b7b01fd5bef712c3a15988a0fe93f8af43
ajlloyd <ajlloyd98@gmail.com>
2022-03-02 22:26:58 +0000

    enable cors header

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ module.exports = {
   env:{
     MONGO_URI: `mongodb+srv://${mongoUsername}:${mongoPassword}@longsword.b2vea.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
   },
+
   async headers() {
     return [
       {
@@ -14,23 +15,14 @@ module.exports = {
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "https://long-sword.herokuapp.com",
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Access-Control-Allow-Origin",
-          },
-        ],
-      },
-    ];
-  },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  }
+  
 
 }
 
