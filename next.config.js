e96532e3f0d2c1796750cd10b266351127318b89
ajlloyd <ajlloyd98@gmail.com>
2022-03-02 22:34:30 +0000

    cors headers added

--- next.config.js
+++ next.config.js
@@ -21,7 +21,16 @@ module.exports = {
         ]
       }
     ]
-  }
+  },
+
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://localhost:3000/:path*',
+      },
+    ]
+  },
   
 
 }
